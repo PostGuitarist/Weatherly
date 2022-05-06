229cfdd37c42f13882a140d12f09f56988ac5e27
Zaden Connell <zaden.connell@gmail.com>
2022-05-06 15:30:16 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Zaden Connell on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
